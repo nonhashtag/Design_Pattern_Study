0aa8a0b97f1072946cb14596f020ef254416b4c2
nonhashtag <75101880+nonhashtag@users.noreply.github.com>
2023-01-17 01:36:49 +0900

    Update Caretaker.h

--- behavior_pattern/memento/Caretaker.h
+++ behavior_pattern/memento/Caretaker.h
@@ -4,9 +4,27 @@
 
 class Caretaker
 {
+public:
+    Caretaker(std::shared_ptr<Originator> pOriginator) : m_pOriginator(pOriginator) { }
+    virtual ~Caretaker() = default;
+
+    void Backup()
+    {
+        std::cout << "\nCaretaker: Saving Originator's state...\n";
+        m_pMementos.push_back(m_pOriginator->Save());
+    }
+
+     void Undo()
+     {
+        if (m_pMementos.empty()) 
+            return;
+        std::shared_ptr<IMemento> pMemento = m_pMementos.back();
+        m_pMementos.pop_back();
+        std::cout << "Caretaker: Restoring state to: " << pMemento->GetName() << "\n";
+     }
 
 
 private:
     std::vector<std::shared_ptr<IMemento>> m_pMementos;
-    std::shared_ptr<Originator> originator;
+    std::shared_ptr<Originator> m_pOriginator;
 };
